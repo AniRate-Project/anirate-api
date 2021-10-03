a91f8b88475a10ee03cbc69873e693d8ea347b05
DanielVip3 <danieledemartino.72004@gmail.com>
2021-10-03 18:13:23 +0200

    Fixed asynchronous problems with module-alias/register importing

--- src/imports.ts
+++ src/imports.ts
@@ -1,13 +1,6 @@
-const optionalImports = [];
-async function optionalImporting() {
-  for (const importable of optionalImports) {
-    await import(importable);
-  }
-}
-
 /* Initialize module-alias, except in dev mode */
 if (process.env.NODE_ENV !== 'development') {
-  optionalImports.push('module-alias/register');
+  require('module-alias/register');
 }
 
 /* Initialize env variables */
